6a515a897ad9b68f2d1745a36a2d06f3fa3233b8
Moa Ha <moahaa03@gmail.com>
2024-06-29 11:50:04 +1200

    add website links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import Image from 'next/image'
 import Head from 'next/head'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import Link from 'next/link'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+config.autoAddCss = false // 필수: CSS 충돌 방지
 
 export default function About() {
   return (
@@ -24,8 +29,13 @@ export default function About() {
         <p>blah blah</p>
       </section>
       <section style={{ display: 'flex', alignItems: 'center' }}>
-        <FontAwesomeIcon icon={faLinkedinIn} className="icon" />
-        <FontAwesomeIcon icon={faGithub} />
+        <Link href="https://www.linkedin.com/in/moa-ha" passHref>
+          <FontAwesomeIcon icon={faLinkedinIn} className="menu-link" />
+        </Link>
+
+        <Link href="https://github.com/moa-ha" passHref>
+          <FontAwesomeIcon icon={faGithub} className="menu-link" />
+        </Link>
       </section>
     </>
   )
