b7365c12627945758965d060f63365668a799271
Moa Ha <moahaa03@gmail.com>
2024-06-29 01:28:14 +1200

    header

---
+++ src/components/header.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Nav from './nav'
+import DarkMode from './darkMode'
+
+import { MouseEventHandler } from 'react'
+
+interface Prop {
+  darkMode: boolean
+  toggleDarkMode: MouseEventHandler<HTMLButtonElement>
+}
+const Header = ({ darkMode, toggleDarkMode }: Prop) => (
+  <header className={`header ${darkMode ? 'dark' : ''}`}>
+    <div className="top-bar container-sm">
+      <div></div>
+      <div className="flex-container">
+        <Nav darkMode={darkMode} />
+        <DarkMode darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      </div>
+    </div>
+  </header>
+)
+
+export default Header
