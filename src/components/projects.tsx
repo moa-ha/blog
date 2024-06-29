b17cb21858626db8dbe3087c294bb74bb1b93e8f
Moa Ha <moahaa03@gmail.com>
2024-06-30 01:54:36 +1200

    project contents

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,60 +1,68 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from 'next/image'
 import React from 'react'
 import '@/styles/projects.css'
 
 const Projects = () => (
-  <div className="item-group">
-    <div className="item">
+  <div className="project-group">
+    <div className="project">
       <Image
-        className="item-image"
+        className="project-image"
         width={200}
         height={200}
         src="/images/carGP.png"
         alt="car-gp"
       />
 
-      <div className="item-content">
-        <h3 className="item-header">Arrowhead Valley Camp</h3>
-        <div className="item-meta">
-          <span className="price">$1200</span>
-          <span className="stay">1 Month</span>
+      <div className="project-content">
+        <h3 className="project-header">Car-GP</h3>
+        <div className="project-meta">
+          <span className="font-bold ">from May 2024 </span>
+          <span className="text-gray-600">-UNDER CONSTRUCTION-</span>
         </div>
-        <div className="item-description"></div>
+        <div className="project-description">
+          An app that tracks car maintenance status and calculates upcoming
+          maintenance schedules based on individual driving distances, sending
+          alerts accordingly.
+        </div>
+        <span className="underline">What I'm Learning ✏️ </span>
+        <div>
+          ✔️ Integrated Auth0 and JWT for authentication, expanding my
+          understanding of secure user authentication.<br></br>
+          ✔️ Improved my design skills to create more professional and
+          user-friendly interfaces.<br></br>
+          ✔️ Continued to broaden my tech stack and enhance my ability to manage
+          a project
+        </div>
       </div>
     </div>
 
-    <div className="item">
+    <div className="project">
       <Image
-        className="item-image"
+        className="project-image"
         width={200}
         height={200}
         src="/images/LyricSync.png"
         alt="LyricSync"
       />
 
-      <div className="item-content">
-        <h3 className="item-header"> Homebrew Stayaway</h3>
-        <div className="item-meta">
-          <span className="price">$1000</span>
-          <span className="stay">2 Weeks</span>
+      <div className="project-content">
+        <h3 className="project-header"> LyricSync</h3>
+        <div className="project-meta">
+          <span className="font-bold ">from May 2024 </span>
+          <span className="text-gray-600">-UNDER CONSTRUCTION-</span>
         </div>
-      </div>
-    </div>
-
-    <div className="item">
-      <Image
-        className="item-image"
-        width={200}
-        height={200}
-        src="/images/tradie-tracker.gif"
-        alt="tradie-tracker"
-      />
-
-      <div className="item-content">
-        <h3 className="item-header">Arrowhead Valley Camp</h3>
-        <div className="item-meta">
-          <span className="price">$1200</span>
-          <span className="stay">1 Month</span>
+        <div className="project-description">
+          App for lyrics collection in translated language.
+        </div>
+        <span className="underline">What I'm Learning ✏️ </span>
+        <div>
+          ✔️ Collaborating weekly with a teammate to develop a full-stack app,
+          gaining deeper familiarity with previously used tools.<br></br>
+          ✔️ Continuously learning and integrating new features, such as unit
+          testing, which I hadn't focused on before.<br></br>
+          ✔️ Enhancing my skills in teamwork, problem-solving, and iterative
+          development.
         </div>
       </div>
     </div>
--- src/components/projectsC.tsx
+++ src/components/projectsC.tsx
@@ -1,59 +1,139 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from 'next/image'
 import React from 'react'
 import '@/styles/projects.css'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
 
 const ProjectsC = () => (
-  <div className="item-group">
-    <div className="item">
+  <div className="project-group">
+    <div className="project">
       <Image
-        className="item-image"
+        className="project-image"
         width={200}
         height={200}
         src="/images/tradie-tracker.gif"
         alt="tradie-tracker"
       />
 
-      <div className="item-content">
-        <h3 className="item-header">Arrowhead Valley Camp</h3>
-        <div className="item-meta">
-          <span className="price">$1200</span>
-          <span className="stay">1 Month</span>
+      <div className="project-content">
+        <h3 className="project-header">
+          Tradie Tracker{' '}
+          <Link href="https://github.com/moa-ha" passHref>
+            <FontAwesomeIcon icon={faGithub} className="menu-link websites" />
+          </Link>
+          <Link href="https://github.com/moa-ha" passHref>
+            <FontAwesomeIcon
+              icon={faCirclePlay}
+              className="menu-link websites"
+            />
+          </Link>
+        </h3>
+        <div className="project-meta">
+          <span className="font-bold ">
+            09 Apr ~ 17 Apr 2024 (group), 21 Apr ~ 10 May 2024 (solo)
+          </span>
+        </div>
+        <div className="project-description">
+          Efficient Work Scheduling App
+          <div>
+            This project is from group project from Dev Academy, and I remade
+            from the scratch to consolidate the concepts and make clear code.
+            The idea was to make a scheduling app using all the tools including
+            front & backend skills we've learnt throughout the bootcamp.
+          </div>
+          <div className="italic">
+            (React / TypeScript / CSS / Restful APIs / Node.js / Express.js /
+            SQLite3 / Knex)
+          </div>
+        </div>
+        <span className="underline">What I Learned ✏️</span>
+        <div>
+          ✔️ Developed a full-stack application from scratch, enhancing my
+          understanding of the entire development process.<br></br>
+          ✔️ Gained experience in project planning, collaborating with team
+          members, and leading meetings.<br></br>
+          ✔️ Improved my ability to implement and manage both frontend and
+          backend components effectively.<br></br>
         </div>
       </div>
     </div>
-    <div className="item">
+    <div className="project">
       <Image
-        className="item-image"
+        className="project-image"
         width={200}
         height={200}
         src="/images/dreamfest.gif"
         alt="dreamfest"
       />
 
-      <div className="item-content">
-        <h3 className="item-header">Arrowhead Valley Camp</h3>
-        <div className="item-meta">
-          <span className="price">$1200</span>
-          <span className="stay">1 Month</span>
+      <div className="project-content">
+        <h3 className="project-header">
+          Dreamfest{' '}
+          <Link href="https://github.com/moa-ha" passHref>
+            <FontAwesomeIcon icon={faGithub} className="menu-link websites" />
+          </Link>
+        </h3>
+
+        <div className="project-meta">
+          <span className="font-bold ">20 Mar ~ 24 Mar 2024</span>
         </div>
-        <div className="item-description"></div>
+        <div className="project-description">
+          Music Festival Dashboard App
+          <div>
+            Dreamfest is a music festival schedule managing app. This was my
+            first app using Knex & SQL Database.
+          </div>
+          <div className="italic">
+            (Express.js / Restful APIs / SQLite3 / Knex)
+          </div>
+        </div>
+        <span className="underline">What I Learned ✏️</span>
+        <div>
+          ✔️ Acquired practical experience integrating SQLite as a database for
+          handling CRUD operations.<br></br>
+          ✔️ Enhanced my understanding of RESTful API design and implementation.
+        </div>
       </div>
     </div>
 
-    <div className="item">
+    <div className="project">
       <Image
-        className="item-image"
+        className="project-image"
         width={200}
         height={200}
         src="/images/pupparazzi.gif"
         alt="pupparazzi"
       />
 
-      <div className="item-content">
-        <h3 className="item-header"> Homebrew Stayaway</h3>
-        <div className="item-meta">
-          <span className="price">$1000</span>
-          <span className="stay">2 Weeks</span>
+      <div className="project-content">
+        <h3 className="project-header">
+          Pupparazzi{' '}
+          <Link href="https://github.com/moa-ha" passHref>
+            <FontAwesomeIcon icon={faGithub} className="menu-link websites" />
+          </Link>
+        </h3>
+        <div className="project-meta">
+          <span className="font-bold ">12 Mar ~ 01 Apr 2024</span>
+        </div>
+        <div className="project-description">
+          Pet Profile Managing App
+          <div>
+            Pupparazz is a pet diary to manage and edit profiles of dogs. This
+            was my first app using Express & JSON API's.
+          </div>
+          <div className="italic">(Express.js / JSON APIs)</div>
+        </div>
+        <span className="underline">What I Learned ✏️</span>
+        <div>
+          ✔️ Gained hands-on experience with CRUD operations using JSON and the
+          file system.<br></br>
+          ✔️ Developed skills in handling data persistence and management in a
+          local environment.<br></br>
+          ✔️ Improved my understanding of working with Express.js to build and
+          manage server-side logic.
         </div>
       </div>
     </div>
