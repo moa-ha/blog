364490a2cd695c12664186bbfe0cff844ff4d835
Moa Ha <moahaa03@gmail.com>
2024-06-29 20:01:14 +1200

    darkmode button styling

--- src/components/darkMode.tsx
+++ src/components/darkMode.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+
 import { MouseEventHandler } from 'react'
 
 interface Prop {
@@ -17,33 +20,14 @@ const DarkMode = ({ darkMode, toggleDarkMode }: Prop) => {
           className={`dark-mode-button ${darkMode ? 'dark' : ''}`}
           onClick={toggleDarkMode}
         >
-          <svg
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            aria-hidden="true"
-            className={darkMode ? 'light-icon hidden' : 'light-icon'}
-          >
-            {/* Light mode icon */}
-            <path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z"></path>
-            <path
-              d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061"
-              fill="none"
-            ></path>
+          {/* Light mode icon */}
+          <svg className={darkMode ? 'light-icon hidden' : 'light-icon'}>
+            <FontAwesomeIcon icon={faSun} />
           </svg>
-          <svg
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-            className={darkMode ? 'dark-icon' : 'dark-icon hidden'}
-          >
-            {/* Dark mode icon */}
-            <path
-              d="M21.764 13.36a8.25 8.25 0 1 1-10.122-10.122 9 9 0 1 0 10.122 10.122z"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></path>
+
+          {/* Dark mode icon */}
+          <svg className={darkMode ? 'dark-icon' : 'dark-icon hidden'}>
+            <FontAwesomeIcon icon={faMoon} />
           </svg>
         </button>
       </div>
