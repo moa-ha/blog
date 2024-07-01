fb87dff0db1926a27431752113671fc9bce15a06
Moa Ha <moahaa03@gmail.com>
2024-07-01 21:16:09 +1200

    loading page

---
+++ src/components/Loader.tsx
@@ -0,0 +1,10 @@
+import '../styles/loader.css'
+
+export default function Loader() {
+  return (
+    <div className="custom-loader">
+      <div className="loader"></div>
+      <p>Loading</p>
+    </div>
+  )
+}
