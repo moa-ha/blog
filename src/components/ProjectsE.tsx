7b473881495853821e3dde272bd6fb5d71b43176
Moa Ha <moahaa03@gmail.com>
2024-08-05 16:24:22 +1200

    add img

--- src/components/ProjectsE.tsx
+++ src/components/ProjectsE.tsx
@@ -15,8 +15,8 @@ const ProjectsE = () => (
           className="wireframe"
           width={200}
           height={200}
-          src="/images/carGp.png"
-          alt="car-gp"
+          src="/images/realisticNotes.gif"
+          alt="Realistic Notes"
         />
         <div className="md:ml-2">
           <h3 className="project-header">
@@ -37,29 +37,44 @@ const ProjectsE = () => (
             </Link>
           </h3>
           <div className=" flex flex-col md:flex-row gap-2">
-            <span className="font-bold ">Terrible Ideas Hackathon</span>
+            <span className="font-bold ">@Terrible Ideas Hackathon</span>
           </div>
-
-          <div className="mt-2 mb-2">
-            <p className="text-base">
-              The first page to check for your car's maintenance!
-            </p>
-            <p>
-              An app that tracks car maintenance status and calculates upcoming
-              maintenance schedules based on individual driving distances,
-              sending alerts accordingly.
-            </p>
+          <div className="mt-2">
+            <span className="underline text-base">✨ Theme: True && Ink </span>
+            <div>
+              Just like in real life where you rarely look back at notes once
+              you've written them down, notes vanishing like disappearing ink?
+              But here’s the twist: enter the correct password, and you get to
+              see the whole note again, just for a moment. It’s all about
+              playing with the theme of ‘ink’ and ‘true’!
+            </div>
           </div>
         </div>
       </div>
-      <div className="mt-2">
-        <span className="underline text-base">✨ Theme: True && Ink </span>
-        <div>
-          Just like in real life where you rarely look back at notes once you've
-          written them down, notes vanishing like disappearing ink? But here’s
-          the twist: enter the correct password, and you get to see the whole
-          note again, just for a moment. It’s all about playing with the theme
-          of ‘ink’ and ‘true’!
+
+      <div className="project-content">
+        <Image
+          className="wireframe"
+          width={200}
+          height={200}
+          src="/images/carGp.png"
+          alt="Ctrl + F"
+        />
+        <div className="md:ml-2">
+          <h3 className="project-header">Ctrl + F</h3>
+          <div className=" flex flex-col md:flex-row gap-2">
+            <span className="font-bold ">@Terrible Ideas Hackathon</span>
+          </div>
+          <div className="mt-2">
+            <span className="underline text-base">✨ Theme: True && Ink </span>
+            <div>
+              Just like in real life where you rarely look back at notes once
+              you've written them down, notes vanishing like disappearing ink?
+              But here’s the twist: enter the correct password, and you get to
+              see the whole note again, just for a moment. It’s all about
+              playing with the theme of ‘ink’ and ‘true’!
+            </div>
+          </div>
         </div>
       </div>
     </div>
